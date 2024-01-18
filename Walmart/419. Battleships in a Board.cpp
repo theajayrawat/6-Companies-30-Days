6a81dbd1318ec9c2c8ebbf8eb38ffec7d36fcb32
Ajay <theajayrawat@gmail.com>
2024-01-18 23:22:03 +0530

    day11

---
+++ Walmart/419. Battleships in a Board.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/battleships-in-a-board/
+// SC: O(N*M)
+// TC: O(N)
+
+class Solution {
+public:
+    	int countBattleships(vector<vector<char>>& board)
+    {
+        int r=board.size(), c=board[0].size();
+        int count=0;
+         for(int i=0; i<r; i++)
+        {
+            for(int j=0; j<c; j++)
+            {
+                if(board[i][j]=='.')
+                    continue;
+                   
+                if(i>0 && board[i-1][j] =='X')
+                   continue;
+                   
+                if(j>0 && board[i][j-1] =='X')
+                   continue;
+                count++;
+            }
+        }
+        return count;
+     }
+};
+
+
