34b3ea613eac19005b1e84c9fcc2efc04c28b155
Ajay <theajayrawat@gmail.com>
2024-01-18 22:53:23 +0530

    day11

---
+++ Walmart/368. Largest Divisible Subset.cpp
@@ -0,0 +1,56 @@
+// https://leetcode.com/problems/largest-divisible-subset/
+// SC: O(N*N)
+// TC: O(N) 
+
+class Solution
+{
+public:
+    vector<int> largestDivisibleSubset(vector<int> &nums)
+    {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<int> dp(n, 1);
+        vector<int> hash(n, 1);
+
+        for (int i = 0; i <= n - 1; i++)
+        {
+
+            hash[i] = i; // initializing with current index
+            for (int prev_index = 0; prev_index < i ; prev_index++)
+            {
+
+                if (nums[i] % nums[prev_index] == 0 && dp[i]<1+dp[prev_index])
+                {
+                    dp[i] = 1 + dp[prev_index];
+                    hash[i] = prev_index;
+                }
+            }
+        }
+
+        int ans = -1;
+        int lastIndex = -1;
+
+        for (int i = 0; i <= n - 1; i++)
+        {
+            if (dp[i] > ans)
+            {
+                ans = dp[i];
+                lastIndex = i;
+            }
+        }
+
+        vector<int> temp;
+        temp.push_back(nums[lastIndex]);
+
+        while (hash[lastIndex] != lastIndex)
+        { // till not reach the initialization value
+            lastIndex = hash[lastIndex];
+            temp.push_back(nums[lastIndex]);
+        }
+
+        // reverse the array
+        reverse(temp.begin(), temp.end());
+        return temp;
+    }
+};
+
